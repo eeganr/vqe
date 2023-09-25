5ada094acdee82c5a5e36c25f352a1a32f88692f
eeganr <whiz.eram@gmail.com>
2023-09-24 20:22:59 -0700

    generalized measurements

--- testvqe.py
+++ testvqe.py
@@ -25,36 +25,53 @@ def apply_many_body_gate(psi_in, gate, nb_qbits, sites):
         psi_out = psi_out.swapaxes(site, -1).squeeze(-1)
     return psi_out# .contiguous()
 
-psi = np.ones((2, 2, 2, 2))
+def measure(psi_in, qubit, basis=[0, 1]):
+    
+    dims = len(psi_in.shape)
+    meas_outcomes = psi_in.shape[qubit]
+
+    slices = [""] * meas_outcomes
+    for i in range(dims):
+        if i == qubit:
+            for j in range(meas_outcomes):
+                slices[j] += f"{j}, "
+        else:
+            for j in range(meas_outcomes):
+                slices[j] += ":, "
+    for slice in slices: 
+        slice = slice[:-2]
+
+    psis = []
+    for i in range(meas_outcomes):
+        psis.append(eval("psi_in[" + slices[i] + "]"))
+
+    probs = [np.linalg.norm(psi) ** 2 for psi in psis]
+
+    psis = [psi / np.linalg.norm(psi) for psi in psis]
+
+    return probs, psis
+    
+
+psi = np.ones((3, 3, 3, 3))
+psi = np.random.randn(3, 3, 3, 3, 3)
 
 psi = psi / np.linalg.norm(psi.ravel())
 
-
+"""
 gate = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, -1]])
 gate = gate.reshape(2, 2, 2, 2)
 
 psiprime = apply_many_body_gate(psi, gate, 4, [0, 2])
+"""
 
-print(psiprime)
+probs, psis = measure(psi, 1)
 
-psi0 = psiprime[:, 0, :, :]
+print(probs)
 
-psi1 = psiprime[:, 1, :, :]
 
-p0 = np.linalg.norm(psi0)
 
-p1 = np.linalg.norm(psi1)
-
-psi0 /= p0
-psi1 /= p1
-
-p0 = p0 ** 2
-p1 = p1 ** 2
-
-print(p0, p1, psi0, psi1)
+# print(psiprime)
 
 # Put in another qubit
 
-psi0 = np.tensordot(psi0, np.array([1, 0]), 0)
-
-print(psi0)
+# psi0 = np.tensordot(psi0, np.array([1, 0]), 0)
