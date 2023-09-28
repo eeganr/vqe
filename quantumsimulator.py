207212840e94270c1ba4c16605f1ac6f58857d6f
eeganr <whiz.eram@gmail.com>
2023-09-27 20:36:24 -0700

    test cases

---
+++ quantumsimulator.py
@@ -0,0 +1,112 @@
+import numpy as bknd
+import numpy as np
+import torch
+from copy import deepcopy
+
+
+class QuantumSimulator:
+    def __init__(self):
+        return
+    
+    @staticmethod
+    def apply_many_body_gate(psi_in, gate, nb_qbits, sites):
+        """
+        psi_in: ibkndut wave function of shape (d,d,d,d,d...)
+        gate: gate of shape (d,d,..., d,d,...) to be applied on psi_in. 
+        nb_qbits: number of qubits of psi_in
+        sites: the sites to apply the gate
+        """
+        num_sites = len(sites)
+        # assert num_sites * 2 == gate.ndim, f''
+        # will not change psi_in anyway
+        psi_out = psi_in
+        # move the contracted dimensions to last for easier manipulation
+        for site in sites:
+            if site < 0:
+                site = nb_qbits + site
+            psi_out = psi_out.reshape(*psi_out.shape, 1).swapaxes(site, -1)
+        psi_out = bknd.tensordot(psi_out, gate, (list(range(-num_sites, 0)), 
+                                                list(range(-num_sites, 0))))
+        # move the contracted dimensions back
+        for site in reversed(sites):
+            if site < 0:
+                site = nb_qbits + site
+            psi_out = psi_out.swapaxes(site, -1).squeeze(-1)
+        return psi_out# .contiguous()
+
+    @staticmethod
+    def measure_remove_single_site(psi_in,  site, basis=None, check_valid=False, eps=1e-14):
+        """
+        psi_in: ibkndut wave function of shape (d,d,d,d,d...)
+        site: the site to measure
+        basis: measurement basis if not provided, will assume to be the z (computational) basis
+            if provided, it should be a matrix, with each row a basis vector (not conjugated). the basis vectors should form an orthonomal basis
+        check_valid: checks if the measurement basis is a valid basis set
+        return: a list of output wave functions (will be combined into an array with one more dimension) with the measured qubit removed, and the corrsponding probabilities
+        """
+        if basis is not None:
+            if check_valid:
+                assert bknd.allclose(basis.T.conj() @ basis, bknd.eye(basis.shape[-1]))
+            # apply gate to site
+            psis_out = bknd.tensordot(basis.conj(), psi_in, axes=(1, site))
+        else:
+            # move site to front
+            psis_out = bknd.moveaxis(psi_in, tuple(range(0, site+1)), (tuple(range(1, site+1)) + (0,)))
+        norms = bknd.linalg.norm(psis_out.reshape(psis_out.shape[0], -1), 2, axis=1)
+        probs = norms**2
+        psis_out = psis_out / norms.reshape(-1, *([1]*len(psis_out.shape[1:]))).clip(eps, None)
+        return probs, psis_out
+
+    @staticmethod
+    def measure_single_site(psi_in, site, basis=None, check_valid=False, eps=1e-14):
+        """
+        psi_in: ibkndut wave function of shape (d,d,d,d,d...)
+        site: the site to measure
+        basis: measurement basis if not provided, will assume to be the z (computational) basis
+            if provided, can be in one of the following forms：
+                1. a matrix, with each row a basis vector (not conjugated). the basis vectors should form an orthonomal basis
+                2. a 3D array, containing matrices of measurement projectors. the measurement projectors should be positive semidefinite (we will not check this), and sum up to identity matrix
+        check_valid: checks if the measurement basis is a valid basis set, will not check for positive semidefinite.
+        return: a list of output wave functions (will be combined into an array with one more dimension), and the corrsponding probabilities
+        """
+        
+        # if basis is form 1
+        if basis is None or len(basis.shape) == 2:
+            probs, psis_out = QuantumSimulator.measure_remove_single_site(psi_in, site, basis, check_valid, eps)
+            # put back the removed qubit
+            psis_out_shape = psis_out.shape
+            # reshape the wave function
+            psis_out = psis_out.reshape(psis_out_shape[0], 1, -1)
+            if basis is None:
+                basis = bknd.eye(psis_out.shape[0])
+                if bknd == torch:
+                    basis = basis.to(psis_out.device)
+            basis = basis[:, :, None]
+            # batch matrix multiplication over the dim with shape 1, which effectively prepend the qubit to the begining
+            psis_out = bknd.matmul(basis, psis_out)
+            # reshape psi back
+            psis_out = psis_out.reshape(psis_out_shape[0], -1, *psis_out_shape[1:])
+            # permute the 0th qubit back to its position
+            psis_out = bknd.moveaxis(psis_out, tuple(range(1, site+2)), (site+1,) + tuple(range(1, site+1))) # +1 because of the batch dimension as the 0th dimension
+            return probs, psis_out
+            
+        # if basis is form 2
+        elif len(basis.shape) == 3:
+            if check_valid:
+                # only check if sums up to identity
+                assert bknd.allclose(basis.sum(0), bknd.eye(basis.shape[-1]))
+            # apply the basis in a batch form and move the axis back to the correct sites
+            psis_out = bknd.moveaxis(bknd.tensordot(basis, psi_in, axes=(2, site)), tuple(range(1, site+2)), (site+1,) + tuple(range(1, site+1))) # +1 because of the batch dimension as the 0th dimension
+            norms = bknd.linalg.norm(psis_out.reshape(psis_out.shape[0], -1), 2, axis=1)
+            probs = norms**2
+            psis_out = psis_out / norms.reshape(-1, *([1]*len(psis_out.shape[1:]))).clip(eps, None)
+            return probs, psis_out
+        
+    @staticmethod
+    def normalize(psi):
+        """
+        psi: wave function of shape (d,d,d,d,d...)\
+        """
+        psi = psi / bknd.linalg.norm(psi.ravel())
+
+        return psi
---
+++ tests.py
@@ -0,0 +1,83 @@
+from quantumsimulator import QuantumSimulator as qs
+import unittest
+import numpy as bknd
+
+class TestSimulator(unittest.TestCase):
+    def test_comp_basis(self):
+        psi = bknd.ones((2, 2, 2)) / bknd.sqrt(8)
+
+        zero = bknd.array([1, 0])
+        plus = bknd.array([1, 1]) / bknd.sqrt(2)
+        right = bknd.array([1, 1j]) / bknd.sqrt(2)
+
+        psi = bknd.kron(bknd.kron(zero, plus), right).reshape(2, 2, 2)
+
+        psi = bknd.ones(3) / bknd.sqrt(3)
+        
+        probs = qs.measure_single_site(psi, 0, check_valid=True)[0]
+
+        # psi = bknd.array([[1, 0], [0, 1]])/bknd.sqrt(2)
+
+        # print(measure_single_site(psi, 0))
+        # print(measure_remove_single_site(psi, 2, check_valid=True)) #, basis=bknd.array([[1, 1], [1j, -1j]])/bknd.sqrt(2)))
+
+        bknd.testing.assert_almost_equal(probs, [1/3, 1/3, 1/3])
+
+        bknd.testing.assert_almost_equal(sum(probs), 1)
+
+    def test_alt_basis(self):
+        psi = bknd.ones((2, 2, 2)) / bknd.sqrt(8)
+
+        zero = bknd.array([1, 0])
+        plus = bknd.array([1, 1]) / bknd.sqrt(2)
+        right = bknd.array([1, 1j]) / bknd.sqrt(2)
+
+        psi = bknd.kron(bknd.kron(zero, plus), right).reshape(2, 2, 2)
+
+        psi = bknd.ones(3) / bknd.sqrt(3)
+        
+        probs = qs.measure_single_site(psi, 0, check_valid=True, basis=bknd.array([[[1, 0, 0], [0, 1, 0], [0, 0, 0]],
+                                                                    #[[0, 0, 0], [0, 1, 0], [0, 0, 0]],
+                                                                    [[0, 0, 0], [0, 0, 0], [0, 0, 1]]]))[0]
+
+        # psi = bknd.array([[1, 0], [0, 1]])/bknd.sqrt(2)
+
+        # print(measure_single_site(psi, 0))
+        # print(measure_remove_single_site(psi, 2, check_valid=True)) #, basis=bknd.array([[1, 1], [1j, -1j]])/bknd.sqrt(2)))
+
+        bknd.testing.assert_almost_equal(probs, [2/3, 1/3], 5)
+        
+        self.assertAlmostEqual(sum(probs), 1)
+        
+    def test_alt_basis2(self):
+        psi = bknd.ones((2, 2, 2)) / bknd.sqrt(8)
+
+        zero = bknd.array([1, 0])
+        plus = bknd.array([1, 1]) / bknd.sqrt(2)
+        right = bknd.array([1, 1j]) / bknd.sqrt(2)
+
+        psi = bknd.kron(bknd.kron(zero, plus), right).reshape(2, 2, 2)
+
+        psi = bknd.ones(3) / bknd.sqrt(3)
+        
+        probs = qs.measure_single_site(psi, 0, check_valid=True, basis=bknd.array([[[1, 0, 0], [0, 1, 0], [0, 0, 0]],
+                                                                    #[[0, 0, 0], [0, 1, 0], [0, 0, 0]],
+                                                                    [[0, 0, 0], [0, 0, 0], [0, 0, 1]]]))[0]
+
+
+        bknd.testing.assert_almost_equal(probs, [2/3, 1/3], 5)
+        
+        self.assertAlmostEqual(sum(probs), 1)
+    
+    def test_random_qiskit(self):
+
+        psi = bknd.random.randn(3, 3, 3, 3, 3)
+
+        v = Statevector(psi)
+
+        print(v.measure())
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
